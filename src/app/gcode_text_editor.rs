6cdc7716e8f5fdff09b40b494155f1b8123e714c
KobayashiRui <roboroborx782@gmail.com>
2023-02-27 18:24:13 +0900

    Update gcode_text_editor.rs
    read file

--- src/app/gcode_text_editor.rs
+++ src/app/gcode_text_editor.rs
@@ -1,6 +1,8 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 use std::{num::NonZeroU64, sync::Arc};
+use std::fs;
+use std::path::Path;
 
 use eframe::{
     egui_wgpu::wgpu::util::DeviceExt,
@@ -13,6 +15,7 @@ use egui_extras::{Size, StripBuilder};
 
 pub struct GcodeTextEditor {
     gcode_data: String,
+    line_data: String,
     selected_row: usize,
     picked_path: Option<String>
 } 
@@ -34,13 +37,14 @@ impl GcodeTextEditor {
 
         Self{
             gcode_data: "G1 X100 \nG1 X200".to_string(),
+            line_data: "1\n2\n".to_string(),
             selected_row: 0,
             picked_path: None,
         }
     }
 
     pub fn update(&mut self, ui: &mut egui::Ui){
-        let Self { gcode_data , selected_row, picked_path} = self;
+        //let Self {mut gcode_data , mut selected_row, mut picked_path} = self;
 
         let dark_mode = ui.visuals().dark_mode;
         let faded_color = ui.visuals().window_fill();
@@ -54,14 +58,32 @@ impl GcodeTextEditor {
         if ui.button("Open file…").clicked() {
             if let Some(path) = rfd::FileDialog::new().pick_file() {
                 self.picked_path = Some(path.display().to_string());
+
                 println!("Update picked path: {:?}", self.picked_path);
-            }
+
+                //load file
+                if let Some(picked_path) = &self.picked_path {
+                    let path_data = Path::new(&picked_path);
+                    match fs::read_to_string(path_data) {
+                        Ok(content) => {
+                            //println!("{}", content);
+                            self.line_data = line_size_to_string(count_lines(&content));
+                            self.gcode_data = content;
+                        }
+                        Err(error) => {println!("Please Select Gcode File")},
+                    }
+                    
+                }
+
+                }
         }
         if let Some(picked_path) = &self.picked_path {
             ui.horizontal(|ui| {
                 ui.label("Picked file:");
                 ui.monospace(picked_path);
+
             });
+
         }
 
 
@@ -69,6 +91,7 @@ impl GcodeTextEditor {
         ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);
 
         egui::ScrollArea::vertical()
+            .max_height(500.0)
             .show_viewport(ui,  |ui, viewport| {
 
         StripBuilder::new(ui)
@@ -78,22 +101,22 @@ impl GcodeTextEditor {
                 strip.strip(|builder| {
                     //builder.sizes(Size::remainder(), 2).horizontal(|mut strip| {
                     builder
-                        .size(Size::exact(50.0))
-                        .size(Size::exact(250.0))
+                        .size(Size::exact(100.0))
+                        .size(Size::exact(400.0))
                         .horizontal(|mut strip| {
                             strip.cell(|ui| {
                                 ui.spacing_mut().item_spacing = egui::vec2(5.0, 0.0);  
                                 //ui.style_mut().visuals.extreme_bg_color = Color32::from_rgb(53, 140, 100);
                                 //ui.style_mut().visuals.override_text_color = Some(Color32::from_rgb(33, 33, 33));
                                 //ui.style_mut().visuals.extreme_bg_color = Color32::from_rgb(164, 192, 176);
-
                                 egui::Frame::none()
                                     .fill(Color32::from_rgb(53, 140, 100))
                                     .show(ui, |ui| {
 
-                                        let mut line_data = &line_size_to_string(count_lines(gcode_data)) as &str;
-                                        let text_edit_output = egui::TextEdit::multiline(&mut line_data)
-                                                                            .min_size(ui.available_size())
+                                        //let mut line_data = &line_size_to_string(count_lines(&self.gcode_data)) as &str;
+                                        let mut l = &self.line_data as &str;
+                                        let text_edit_output = egui::TextEdit::multiline(&mut l)
+                                                                            //.min_size(ui.available_size())
                                                                             //.layouter(&mut layouter)
                                                                             .show(ui);
 
@@ -108,7 +131,6 @@ impl GcodeTextEditor {
                                         );
 
                                     });
-
                                 //ui.label(line_size_to_string(count_lines(gcode_data)));
                                 //ui.add(egui::Label::new(line_size_to_string(count_lines(gcode_data))));
                                 //ui.painter().rect_filled(
@@ -140,11 +162,24 @@ impl GcodeTextEditor {
                                         },);
                                     ui.fonts(|f| f.layout_job(layout_job))
                                 };
-                                let text_edit_output = egui::TextEdit::multiline(gcode_data)
-                                                                    .min_size(ui.available_size())
+                                let text_edit_output = egui::TextEdit::multiline(&mut self.gcode_data)
+                                                                    //.min_size(ui.available_size())
+                                                                    .desired_rows(10)
+                                                                    .desired_width(f32::INFINITY)
                                                                     //.layouter(&mut layouter)
                                                                     .show(ui);
                                 //let output = ui.add_sized(ui.available_size(), egui::TextEdit::multiline(gcode_data));
+
+                                let rect_data = text_edit_output.response.rect;
+                                let row_num = self.selected_row as f32;
+                                ui.painter().rect_filled(
+                                    egui::Rect::from_min_max(egui::Pos2::new(rect_data.left(), rect_data.top()+(row_num*14.0)+1.0 ), egui::Pos2::new(rect_data.right(), rect_data.top()+(row_num*14.0)+1.0+14.0)),
+                                    0.0,
+                                    Color32::from_rgba_premultiplied(100, 0, 0, 20),
+                                    //faded_color(Color32::from_rgb(77, 163, 109)),
+                                    //Color32::from_rgb(164, 192, 176),
+                                );
+
                                 let cursor_pos = text_edit_output.cursor_range;
                                 if let Some(cursor_pos) = cursor_pos {
                                     //let rect_data = text_edit_output.response.rect;
@@ -160,23 +195,6 @@ impl GcodeTextEditor {
                                 }
 
 
-                                let rect_data = text_edit_output.response.rect;
-                                let row_num = self.selected_row as f32;
-                                ui.painter().rect_filled(
-                                    egui::Rect::from_min_max(egui::Pos2::new(rect_data.left(), rect_data.top()+(row_num*14.0)+1.0 ), egui::Pos2::new(rect_data.right(), rect_data.top()+(row_num*14.0)+1.0+14.0)),
-                                    0.0,
-                                    Color32::from_rgba_premultiplied(100, 0, 0, 20),
-                                    //faded_color(Color32::from_rgb(77, 163, 109)),
-                                    //Color32::from_rgb(164, 192, 176),
-                                );
-
-
-
-
-                                let click_enter = ui.input(|i| i.key_pressed(egui::Key::Enter));
-                                if click_enter {
-                                    println!("Enter : {}", click_enter);
-                                }
 
 
                                 //let hover_pos = ui.input().pointer.hover_pos();
