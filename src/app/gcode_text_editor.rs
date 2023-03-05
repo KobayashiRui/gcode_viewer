82b50b566793e0b7d427797eee61ce52502a8172
KobayashiRui <roboroborx782@gmail.com>
2023-03-05 21:52:39 +0900

    Update gcode_text_editor.rs

--- src/app/gcode_text_editor.rs
+++ src/app/gcode_text_editor.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use core::num;
 use std::{num::NonZeroU64, sync::Arc};
 use std::fs;
 use std::path::Path;
@@ -7,6 +8,7 @@ use std::path::Path;
 use eframe::{
     egui_wgpu::wgpu::util::DeviceExt,
     egui_wgpu::{self, wgpu},
+    epaint,
 };
 
 use eframe::egui;
@@ -17,7 +19,9 @@ pub struct GcodeTextEditor {
     gcode_data: String,
     line_data: String,
     selected_row: usize,
-    picked_path: Option<String>
+    text_slider: i32,
+    picked_path: Option<String>,
+    text_row: Vec<String>,
 } 
 
 fn count_lines(s: &String) -> usize {
@@ -34,12 +38,21 @@ fn line_size_to_string(s: usize) -> String {
 
 impl GcodeTextEditor {
     pub fn new() -> Self{
+        let mut text_row: Vec<String> = vec![];
+        let num_rows = 50000;
+        for row in 0..num_rows {
+            let mut text = format!("This is row {}/{}", row + 1, num_rows);
+            text_row.push(text);
+            //egui::TextEdit::singleline(&mut text).show(ui);
+        }
 
         Self{
             gcode_data: "G1 X100 \nG1 X200".to_string(),
             line_data: "1\n2\n".to_string(),
             selected_row: 0,
+            text_slider: 0,
             picked_path: None,
+            text_row: text_row,
         }
     }
 
@@ -90,9 +103,10 @@ impl GcodeTextEditor {
 
         ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);
 
+        /* 
         egui::ScrollArea::vertical()
             .max_height(500.0)
-            .show_viewport(ui,  |ui, viewport| {
+            .show(ui,  |ui| {
 
         StripBuilder::new(ui)
             .size(Size::remainder())
@@ -103,6 +117,7 @@ impl GcodeTextEditor {
                     builder
                         .size(Size::exact(100.0))
                         .size(Size::exact(400.0))
+                        .size(Size::exact(20.0))
                         .horizontal(|mut strip| {
                             strip.cell(|ui| {
                                 ui.spacing_mut().item_spacing = egui::vec2(5.0, 0.0);  
@@ -148,25 +163,29 @@ impl GcodeTextEditor {
                                 ui.style_mut().visuals.override_text_color = Some(Color32::from_rgb(33, 33, 33));
                                 let mut layouter = |ui: &egui::Ui, string: &str, wrap_width: f32| {
                                     let mut layout_job = egui::text::LayoutJob::default();
-                                    layout_job.justify = true;
-                                    layout_job.wrap.max_width = wrap_width;
+
+                                    let start_pos = (self.text_slider * 10) as usize;
+
                                     layout_job.append(
-                                        string,
+                                        &string[..],
                                         0.0,     
                                         egui::TextFormat {
                                             font_id: egui::FontId::new(12.0, egui::FontFamily::Monospace),
                                             color: Color32::BLACK,
                                             //background: Color32::RED,
-
                                             ..Default::default()
                                         },);
+                                    
+                                    
                                     ui.fonts(|f| f.layout_job(layout_job))
                                 };
+
+                                
                                 let text_edit_output = egui::TextEdit::multiline(&mut self.gcode_data)
                                                                     //.min_size(ui.available_size())
                                                                     .desired_rows(10)
                                                                     .desired_width(f32::INFINITY)
-                                                                    //.layouter(&mut layouter)
+                                                                    .layouter(&mut layouter)
                                                                     .show(ui);
                                 //let output = ui.add_sized(ui.available_size(), egui::TextEdit::multiline(gcode_data));
 
@@ -180,11 +199,38 @@ impl GcodeTextEditor {
                                     //Color32::from_rgb(164, 192, 176),
                                 );
 
+
                                 let cursor_pos = text_edit_output.cursor_range;
+
+
                                 if let Some(cursor_pos) = cursor_pos {
+
+                                    self.selected_row = cursor_pos.primary.rcursor.row;
+
+
+                                    //if text_edit_output.response.clicked(){
+                                    //    println!("click!");
+
+                                    //    let text_edit_id = text_edit_output.response.id;
+                                    //    if let Some(mut state) = egui::TextEdit::load_state(ui.ctx(), text_edit_id) {
+                                    //        println!("{}", cursor_pos.primary.ccursor.index );
+                                    //        let ccursor = egui::text::CCursor::new(cursor_pos.primary.ccursor.index + (self.text_slider * 10) as usize);
+                                    //        state.set_ccursor_range(Some(egui::text::CCursorRange::one(ccursor)));
+                                    //        println!("{}", cursor_pos.primary.ccursor.index );
+                                    //        state.store(ui.ctx(), text_edit_id);
+                                    //        //ui.ctx().memory().request_focus(text_edit_id); // give focus back to the `TextEdit`.
+                                    //    }
+                                    //}
+
                                     //let rect_data = text_edit_output.response.rect;
                                     //let row_num = cursor_pos.primary.rcursor.row as f32;
-                                    self.selected_row = cursor_pos.primary.rcursor.row;
+
+                                    //let mut now_ccursor = cursor_pos.primary.ccursor;
+
+                                    //text_edit_output.state.set_cursor_range(Some(cursor_pos));
+                                    //text_edit_output.state.set_ccursor_range( Some(egui::widgets::text_edit::CCursorRange::one(egui::text::CCursor::new(cursor_pos.primary.ccursor.index + (self.text_slider * 10) as usize))));
+                                    //text_edit_output.state.set_ccursor_range( Some(egui::widgets::text_edit::CCursorRange::one(egui::text::CCursor::new(0))));
+
                                     //ui.painter().rect_filled(
                                     //    egui::Rect::from_min_max(egui::Pos2::new(rect_data.left(), rect_data.top()+(row_num*14.0)+1.0 ), egui::Pos2::new(rect_data.right(), rect_data.top()+(row_num*14.0)+1.0+14.0)),
                                     //    0.0,
@@ -212,38 +258,45 @@ impl GcodeTextEditor {
                                 //    }
                                 //}
                             });
+
+                            strip.cell(|ui| {
+                                //let mut text_slider = 0;
+                                ui.add(
+                                    egui::Slider::new(&mut self.text_slider, (100 as i32)..=(0 as i32))
+                                        .orientation(egui::SliderOrientation::Vertical)
+                                        .text("i32 demo slider")
+                                );
+                                //println!("output: {}", self.text_slider);
+                            })
                     });
                 });
-                /*
-                strip.strip(|builder| {
-                    builder
-                        .size(Size::remainder())
-                        .size(Size::remainder())
-                        .horizontal(|mut strip| {
-                            strip.cell(|ui| {
-                                ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);  
-                                ui.painter().rect_filled(
-                                    ui.available_rect_before_wrap(),
-                                    0.0,
-                                    faded_color(Color32::BLUE),
-                                );
-                                ui.label("width: 100%\nheight: 50px");
-                            });
-                            strip.cell(|ui| {
-                                ui.spacing_mut().item_spacing = egui::vec2(0.0, 0.0);  
-                                ui.painter().rect_filled(
-                                    ui.available_rect_before_wrap(),
-                                    0.0,
-                                    faded_color(Color32::BLUE),
-                                );
-                                ui.label("width: 100%\nheight: 50px");
-                            });
-                    });
-                });
-                */
             });
 
         });
+        */
+
+        //let text_edit_output = egui::TextEdit::multiline(&mut self.gcode_data)
+        //                                    .min_size(ui.available_size())
+        //                                    .desired_rows(10)
+        //                                    //.desired_width(f32::INFINITY)
+        //                                    .show(ui);
+
+
+        let text_style = egui::TextStyle::Body;
+        let row_height = ui.text_style_height(&text_style);
+        let num_rows = 50000;
+        egui::ScrollArea::vertical().auto_shrink([false; 2]).show_rows(
+            ui,
+            row_height,
+            num_rows,
+            |ui, row_range| {
+                for row in row_range {
+                    //let mut text = format!("This is row {}/{}", row + 1, num_rows);
+                    egui::TextEdit::singleline(&mut self.text_row[row]).show(ui);
+                }
+            },
+        );
+
         //egui::Grid::new("gcode_data")
         //    .spacing(egui::Vec2::new(0.0, 0.0))
         //    .striped(true)
